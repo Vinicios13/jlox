9c0c8ac71513b9ea437a391e8c92bb464c18d14f
vincios <vinicios.a.ferreira13@gmail.com>
2019-08-21 22:19:04 -0300

    Code restructuring

--- src/lox/Scanner.java
+++ src/lox/Scanner.java
@@ -1,9 +1,11 @@
+package lox;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import static com.craftinginterpreters.lox.TokenType.*;
+import static lox.TokenType.*;
 
 class Scanner {
     private final String source;
