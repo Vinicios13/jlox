4e7be4523cc5ed9292d9928b0eb55966ff0883f2
vincios <vinicios.a.ferreira13@gmail.com>
2019-10-06 23:12:48 -0300

    implementation of variables

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 // class Interpreter implements Expr.Visitor<Object> {
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+  private Environment environment = new Environment();
 
   void interpret(List<Stmt> statements) {
     try {
@@ -51,6 +52,30 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     Object value = evaluate(stmt.expression);
     System.out.println(stringify(value));
     return null;
+  }
+
+  @Override
+  public Void visitVarStmt(Stmt.Var stmt) {
+    Object value = null;
+    if (stmt.initializer != null) {
+      value = evaluate(stmt.initializer);
+    }
+
+    environment.define(stmt.name.lexeme, value);
+    return null;
+  }
+
+  @Override
+  public Object visitVariableExpr(Expr.Variable expr) {
+    return environment.get(expr.name);
+  }
+
+  @Override
+  public Object visitAssignExpr(Expr.Assign expr) {
+    Object value = evaluate(expr.value);
+
+    environment.assign(expr.name, value);
+    return value;
   }
 
   @Override
@@ -106,12 +131,31 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     return null;
   }
 
+  @Override
+  public Void visitBlockStmt(Stmt.Block stmt) {
+    executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
   private Object evaluate(Expr expr) {
     return expr.accept(this);
   }
 
   private void execute(Stmt stmt) {
     stmt.accept(this);
+  }
+
+  void executeBlock(List<Stmt> statements, Environment environment) {
+    Environment previous = this.environment;
+    try {
+      this.environment = environment;
+
+      for (Stmt statement : statements) {
+        execute(statement);
+      }
+    } finally {
+      this.environment = previous;
+    }
   }
 
   private boolean isTruthy(Object object) {
