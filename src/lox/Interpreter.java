6fa20bff137e54e5b73a19a02a4fdde32454775d
vincios <vinicios.a.ferreira13@gmail.com>
2019-10-08 20:20:15 -0300

    implementation of classes

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-// class Interpreter implements Expr.Visitor<Object> {
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
@@ -61,6 +60,24 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   }
 
   @Override
+  public Object visitSetExpr(Expr.Set expr) {
+    Object object = evaluate(expr.object);
+
+    if (!(object instanceof LoxInstance)) {
+      throw new RuntimeError(expr.name, "Only instances have fields.");
+    }
+
+    Object value = evaluate(expr.value);
+    ((LoxInstance) object).set(expr.name, value);
+    return value;
+  }
+
+  @Override
+  public Object visitThisExpr(Expr.This expr) {
+    return lookUpVariable(expr.keyword, expr);
+  }
+
+  @Override
   public Object visitGroupingExpr(Expr.Grouping expr) {
     return evaluate(expr.expression);
   }
@@ -88,7 +105,7 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Void visitFunctionStmt(Stmt.Function stmt) {
-    LoxFunction function = new LoxFunction(stmt, environment);
+    LoxFunction function = new LoxFunction(stmt, environment, false);
     environment.define(stmt.name.lexeme, function);
     return null;
   }
@@ -156,7 +173,14 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
 
-    environment.assign(expr.name, value);
+    Integer distance = locals.get(expr);
+
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
@@ -239,6 +263,31 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   @Override
   public Void visitBlockStmt(Stmt.Block stmt) {
     executeBlock(stmt.statements, new Environment(environment));
+    return null;
+  }
+
+  @Override
+  public Object visitGetExpr(Expr.Get expr) {
+    Object object = evaluate(expr.object);
+    if (object instanceof LoxInstance) {
+      return ((LoxInstance) object).get(expr.name);
+    }
+
+    throw new RuntimeError(expr.name, "Only instances have properties.");
+  }
+
+  @Override
+  public Void visitClassStmt(Stmt.Class stmt) {
+    environment.define(stmt.name.lexeme, null);
+
+    Map<String, LoxFunction> methods = new HashMap<>();
+    for (Stmt.Function method : stmt.methods) {
+      LoxFunction function = new LoxFunction(method, environment, method.name.lexeme.equals("init"));
+      methods.put(method.name.lexeme, function);
+    }
+
+    LoxClass klass = new LoxClass(stmt.name.lexeme, methods);
+    environment.assign(stmt.name, klass);
     return null;
   }
 
--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -15,12 +15,15 @@ public class GenerateAst {
 
         defineAst(outputDir, "Expr",
                 Arrays.asList("Assign   : Token name, Expr value", "Binary   : Expr left, Token operator, Expr right",
-                        "Call     : Expr callee, Token paren, List<Expr> arguments", "Grouping : Expr expression",
-                        "Literal  : Object value", "Logical  : Expr left, Token operator, Expr right",
+                        "Call     : Expr callee, Token paren, List<Expr> arguments",
+                        "Get      : Expr object, Token name", "Grouping : Expr expression", "Literal  : Object value",
+                        "Logical  : Expr left, Token operator, Expr right",
+                        "Set      : Expr object, Token name, Expr value", "This     : Token keyword",
                         "Unary    : Token operator, Expr right", "Variable : Token name"));
 
         defineAst(outputDir, "Stmt",
                 Arrays.asList("Block      : List<Stmt> statements", "Expression : Expr expression",
+                        "Class      : Token name, List<Stmt.Function> methods",
                         "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                         "Function   : Token name, List<Token> params, List<Stmt> body", "Print      : Expr expression",
                         "Return     : Token keyword, Expr value", "Var        : Token name, Expr initializer",
